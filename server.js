99325796fe77665104e54a306d824efb602fd49f
Jonathan Sosin <jonysosin@gmail.com>
2018-09-23 21:53:19 -0300

    test

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 })
 
-// io.on('message', (message) => {
-//   io.emit('message', message);
-// })
+socket.on('message', (message) => {
+  io.emit('message', message);
+})
 
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-setInterval(() => io.emit('message', new Date().toTimeString()), 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('message', new Date().toTimeString()), 1000);
