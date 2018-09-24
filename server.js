a946bbf6a77f067506653834ff2233d83d969adf
Jonathan Sosin <jonysosin@gmail.com>
2018-09-23 21:59:52 -0300

    A

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-})
+  io.emit('message', 'El usuario ' + socket.id + ' se ha conectado')
 
-socket.on('message', (message) => {
-  io.emit('message', message);
+  socket.on('message', function(msg){
+		socket.emit('message', mss); // Todos menos yo
+	});
 })
-
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 // setInterval(() => io.emit('message', new Date().toTimeString()), 1000);
